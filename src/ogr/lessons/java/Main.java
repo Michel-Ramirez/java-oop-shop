6dc12ce0bfdd5f60fd8df2de27255c76c2df8668
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-11-13 15:22:32 +0100

    first commit

---
+++ src/ogr/lessons/java/Main.java
@@ -0,0 +1,13 @@
+package ogr.lessons.java;
+
+import ogr.lessons.java.hop.Prodotto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
